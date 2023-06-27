8f1fc53f74b7df7e81022be6e0cdc0fe327280c5
FreeLunchTrading <64173645+0xFLT@users.noreply.github.com>
2023-06-27 18:43:16 -0500

    Update MealTicket.sol

--- MealTicket.sol
+++ MealTicket.sol
@@ -2,9 +2,9 @@
 
 pragma solidity >=0.8.0;
 
-import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
-import "@chainlink/contracts/src/v0.6/vendor/SafeMathChainlink.sol";
 
-contract FundMe {
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+
+contract Quarters {
 
 }
